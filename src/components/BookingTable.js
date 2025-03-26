3a738b770a861e9e3f7b48026baf619d2a911535
marcosrobvel <marcosraranjuez@gmail.com>
2025-03-26 12:53:25 +0100

    BookingTable_Styled

---
+++ src/components/BookingTable.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { TableCell } from "@mui/material";
+
+export const StyledTableCell = styled(TableCell)`
+    &.MuiTableCell-root, &.MuiTableCell-body {
+        font-family: 'Poppins';
+    }
+`;
--- src/components/BookingTable.jsx
+++ src/components/BookingTable.jsx
@@ -1,48 +1,50 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { StyledTableCell } from './BookingTable';
+import { Table } from '@mui/material';
+
 
 export default function BookingTable(props) {
   return (
     <TableContainer component={Paper} sx={{ maxWidth: '100%', margin: 'auto', boxShadow: 3 }}>
-      <Table sx={{ minWidth: 650 }} aria-label="booking table">
+      <Table sx={{ minWidth: 650 }} aria-label="booking table" >
         <TableHead>
           <TableRow>
-            <TableCell sx={{ fontWeight: 'bold' }}>Guest</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Order Date</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Check In</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Check Out</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Special Request</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Room Type</TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>Status</TableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Guest</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Order Date</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Check In</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Check Out</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Special Request</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Room Type</StyledTableCell>
+            <StyledTableCell sx={{ fontWeight: 'bold' }}>Status</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {Array.isArray(props.bookingsData) && props.bookingsData.length > 0 ? (
             props.bookingsData.map((booking) => (
               <TableRow key={booking.id}>
-                <TableCell>
+                <StyledTableCell>
                   {booking.guest}
                   <p>#{booking.id}</p>
-                </TableCell>
-                <TableCell>{booking.orderDate}</TableCell>
-                <TableCell>{booking.checkIn}</TableCell>
-                <TableCell>{booking.checkOut}</TableCell>
-                <TableCell>{booking.special}</TableCell>
-                <TableCell>{booking.roomType} - {booking.roomNumber}</TableCell>
-                <TableCell>{booking.bookStatus}</TableCell>
+                </StyledTableCell>
+                <StyledTableCell>{booking.orderDate}</StyledTableCell>
+                <StyledTableCell>{booking.checkIn}</StyledTableCell>
+                <StyledTableCell>{booking.checkOut}</StyledTableCell>
+                <StyledTableCell>{booking.special}</StyledTableCell>
+                <StyledTableCell>{booking.roomType} - {booking.roomNumber}</StyledTableCell>
+                <StyledTableCell>{booking.bookStatus}</StyledTableCell>
               </TableRow>
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={8} align="center">
+              <StyledTableCell colSpan={8} align="center">
                 No hay reservas disponibles.
-              </TableCell>
+              </StyledTableCell>
             </TableRow>
           )}
         </TableBody>
