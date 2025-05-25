2d17f571ea2cf5216110c2e14769310bccf19290
marcosrobvel <marcosraranjuez@gmail.com>
2025-05-25 16:45:26 +0200

    corrections-dashboard

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookingReducer from '../features/bookingsSlice';
+import roomsReducer from '../features/roomsSlice';
+import contactsReducer from '../features/contactsSlice';
+import usersReducer from '../features/usersSlice';
+import rooms from '../data/rooms';
+import { users } from '../data/users';
 
 export const store = configureStore({
   reducer: {
     booking: bookingReducer,
+    rooms: roomsReducer,
+    contacts: contactsReducer,
+    users: usersReducer
   },
   devTools: true as boolean,
 });
